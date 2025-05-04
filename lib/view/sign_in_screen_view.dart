31d5a2197dbc25a554084786a780f0b9f5835b5a
mzee-flutter <mudair.khan37@gmail.com>
2025-05-04 21:18:40 +0400

    this is the basic login page view or screen

---
+++ lib/view/sign_in_screen_view.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:right_case/resources/custom_text_fields.dart';
+import 'package:right_case/resources/login_icons.dart';
+import 'package:right_case/routes/routes_names.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.symmetric(horizontal: 20.w),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                minHeight: MediaQuery.of(context).size.height,
+              ),
+              child: IntrinsicHeight(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment:
+                      CrossAxisAlignment.stretch, // for horizontal
+                  children: [
+                    Text(
+                      'Welcome!',
+                      style: TextStyle(
+                        color: Color(0xFF0077B5),
+                        fontSize: 24.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 10.h),
+                    Text(
+                      'Login to your existing account',
+                      style: TextStyle(
+                        fontSize: 16.sp,
+                        color: Colors.grey[600],
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 15.h),
+
+                    // Email
+                    CustomTextField.fieldLabel(
+                        'Enter your email, phone, or username'),
+                    SizedBox(height: 5.h),
+                    CustomTextField(hint: 'example@gmail.com'),
+                    SizedBox(height: 15.h),
+
+                    // Password
+                    CustomTextField.fieldLabel('Enter your password'),
+                    SizedBox(height: 5.h),
+                    CustomTextField(hint: 'Password'),
+
+                    // Login button
+                    SizedBox(height: 20.h),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, RoutesName.homeScreen);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF0077B5),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.r),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 14.h),
+                      ),
+                      child: Text(
+                        'Login',
+                        style: TextStyle(
+                          fontSize: 18.sp,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+
+                    // OR divider
+                    SizedBox(height: 20.h),
+                    Row(
+                      children: [
+                        Expanded(
+                            child:
+                                Divider(color: Colors.grey[400], thickness: 1)),
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10.w),
+                          child: Text(
+                            'Or login with',
+                            style: TextStyle(
+                                fontSize: 14.sp, color: Colors.grey[600]),
+                          ),
+                        ),
+                        Expanded(
+                            child:
+                                Divider(color: Colors.grey[400], thickness: 1)),
+                      ],
+                    ),
+
+                    // Social icons
+                    SizedBox(height: 20.h),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        LoginIcons(assetPath: 'images/google1.png'),
+                        SizedBox(width: 20.w),
+                        LoginIcons(assetPath: 'images/facebook.png'),
+                        SizedBox(width: 20.w),
+                        LoginIcons(assetPath: 'images/apple.png'),
+                      ],
+                    ),
+
+                    // Sign Up Link
+                    SizedBox(height: 30.h),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, RoutesName.signUpScreen);
+                      },
+                      child: Center(
+                        child: RichText(
+                          text: TextSpan(
+                            text: "Don't have an account yet? ",
+                            style: TextStyle(
+                                fontSize: 14.sp, color: Colors.grey[600]),
+                            children: [
+                              TextSpan(
+                                text: 'Sign Up',
+                                style: TextStyle(
+                                  fontSize: 14.sp,
+                                  color: const Color(0xFF0077B5),
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
