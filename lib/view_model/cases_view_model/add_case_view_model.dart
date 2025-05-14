6d6e8f676df360afd99f37eb3c596c5c2a3311d6
mzee-flutter <mudair.khan37@gmail.com>
2025-05-14 11:17:36 +0400

    this is the view_model class the provide the methods for adding the deleting for the cases

---
+++ lib/view_model/cases_view_model/add_case_view_model.dart
@@ -0,0 +1,48 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:right_case/models/case_model.dart';
+import 'package:right_case/view_model/cases_view_model/case_view_model.dart';
+import 'package:uuid/uuid.dart';
+
+class AddCaseViewModel with ChangeNotifier {
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descController = TextEditingController();
+  final TextEditingController clientIdController = TextEditingController();
+  final TextEditingController statusController = TextEditingController();
+
+  void submitCase(context) {
+    final title = titleController.text.trim();
+    final description = descController.text.trim();
+    final clientID = clientIdController.text.trim();
+    final status = statusController.text.trim();
+
+    if (title.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Center(child: Text('Name is required'))),
+      );
+      return;
+    }
+
+    final newCase = CaseModel(
+      id: Uuid().v4(),
+      title: title,
+      description: description,
+      clientId: clientID,
+      status: status,
+    );
+
+    Provider.of<CaseViewModel>(context, listen: false).addCase(newCase);
+    Navigator.pop(context);
+  }
+
+  void clearFields() {
+    titleController.clear();
+    descController.clear();
+    clientIdController.clear();
+    statusController.clear();
+    notifyListeners();
+  }
+}
