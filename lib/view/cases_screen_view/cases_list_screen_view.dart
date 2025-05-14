f392325710d5c994d49dead28d14fbb594f199d7
mzee-flutter <mudair.khan37@gmail.com>
2025-05-14 22:02:48 +0400

    adding some more detail to this page in the card

--- lib/view/cases_screen_view/cases_list_screen_view.dart
+++ lib/view/cases_screen_view/cases_list_screen_view.dart
@@ -1,84 +1,70 @@
-// screens/cases/cases_list_screen.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
-
-import '../../view_model/case_view_model.dart';
+import 'package:right_case/resources/case_info_card.dart';
+import 'package:right_case/routes/routes_names.dart';
+import 'package:right_case/view_model/cases_view_model/case_view_model.dart';
 
 class CasesListScreen extends StatelessWidget {
   const CasesListScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final caseVM = context.watch<CaseViewModel>();
-    final titleController = TextEditingController();
-    final descController = TextEditingController();
-    final clientIdController = TextEditingController();
-    final statusController = TextEditingController();
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("Cases"),
         backgroundColor: Colors.grey.shade300,
       ),
-      body: ListView.builder(
-        itemCount: caseVM.cases.length,
-        itemBuilder: (context, index) {
-          final caseItem = caseVM.cases[index];
-          return ListTile(
-            title: Text(caseItem.title),
-            subtitle: Text(caseItem.status),
+      body: Consumer<CaseViewModel>(
+        builder: (context, caseVM, child) {
+          if (caseVM.cases.isEmpty) {
+            return Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 100.h,
+                    width: 100.w,
+                    decoration: BoxDecoration(
+                        color: Colors.grey.shade300, shape: BoxShape.circle),
+                    child: Icon(
+                      Icons.report_problem_rounded,
+                      size: 40,
+                      color: Colors.grey.shade500,
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'Cases Not Found',
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          }
+          return ListView.builder(
+            itemCount: caseVM.cases.length,
+            itemBuilder: (context, index) {
+              final clientCase = caseVM.cases[index];
+
+              return CaseInfoCard(clientCase: clientCase);
+            },
           );
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
         backgroundColor: Colors.grey.shade800,
-        onPressed: () => showDialog(
-          context: context,
-          builder: (_) => AlertDialog(
-            title: const Text("Add Case"),
-            content: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextField(
-                    controller: titleController,
-                    decoration: const InputDecoration(labelText: 'Title')),
-                TextField(
-                    controller: descController,
-                    decoration:
-                        const InputDecoration(labelText: 'Description')),
-                TextField(
-                    controller: clientIdController,
-                    decoration: const InputDecoration(labelText: 'Client ID')),
-                TextField(
-                    controller: statusController,
-                    decoration: const InputDecoration(labelText: 'Status')),
-              ],
-            ),
-            actions: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  caseVM.addCase(titleController.text, descController.text,
-                      clientIdController.text, statusController.text);
-                  Navigator.pop(context);
-                },
-                icon: const Icon(Icons.cases_rounded, color: Colors.white),
-                label: const Text(
-                  'Add Case',
-                  style: TextStyle(color: Colors.white),
-                ),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.grey.shade800,
-                  minimumSize: const Size(double.infinity, 48),
-                ),
-              ),
-            ],
-          ),
-        ),
         icon: const Icon(Icons.cases_rounded, color: Colors.white),
         label: const Text(
           'Add Case',
           style: TextStyle(color: Colors.white),
         ),
+        onPressed: () {
+          Navigator.pushNamed(context, RoutesName.addCaseScreen);
+        },
       ),
     );
   }
