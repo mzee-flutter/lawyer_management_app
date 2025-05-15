d912a7d9589e011e2f3fea1780b2c256557a5df7
mzee-flutter <mudair.khan37@gmail.com>
2025-05-15 21:13:59 +0400

    adding the delete method for the cases

--- lib/view_model/cases_view_model/case_view_model.dart
+++ lib/view_model/cases_view_model/case_view_model.dart
@@ -1,20 +1,17 @@
 import 'package:flutter/material.dart';
 import '../../models/case_model.dart';
-import 'package:uuid/uuid.dart';
 
 class CaseViewModel extends ChangeNotifier {
   final List<CaseModel> _cases = [];
   List<CaseModel> get cases => _cases;
 
-  void addCase(
-      String title, String description, String clientId, String status) {
-    _cases.add(CaseModel(
-      id: const Uuid().v4(),
-      title: title,
-      description: description,
-      clientId: clientId,
-      status: status,
-    ));
+  void addCase(CaseModel clientCase) {
+    _cases.add(clientCase);
+    notifyListeners();
+  }
+
+  void removeCase(CaseModel clientCase) {
+    _cases.remove(clientCase);
     notifyListeners();
   }
 }
