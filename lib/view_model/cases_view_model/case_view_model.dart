d1a58fcca50f639f890854f72cacc6016b62a920
mzee-flutter <mudair.khan37@gmail.com>
2025-05-16 10:19:23 +0400

    addin the update case method

--- lib/view_model/cases_view_model/case_view_model.dart
+++ lib/view_model/cases_view_model/case_view_model.dart
@@ -1,17 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import '../../models/case_model.dart';
 
 class CaseViewModel extends ChangeNotifier {
-  final List<CaseModel> _cases = [];
-  List<CaseModel> get cases => _cases;
+  String _searchQuery = '';
+  String get searchQuery => _searchQuery;
+
+  List<CaseModel> _filteredCases = [];
+
+  List<CaseModel> get filteredCase => _filteredCases;
+
+  CaseViewModel() {
+    _init();
+  }
+
+  void _init() {
+    Hive.box<CaseModel>('Cases').listenable().addListener(_applyFilter);
+    _applyFilter();
+  }
 
   void addCase(CaseModel clientCase) {
-    _cases.add(clientCase);
+    final box = Hive.box<CaseModel>('cases');
+    box.put(clientCase.id, clientCase);
     notifyListeners();
   }
 
   void removeCase(CaseModel clientCase) {
-    _cases.remove(clientCase);
+    clientCase.delete();
+    notifyListeners();
+  }
+
+  void updateCase(CaseModel clientCase) {
+    final box = Hive.box<CaseModel>('cases');
+    box.put(clientCase.id, clientCase);
+  }
+
+  void _applyFilter() {
+    final box = Hive.box<CaseModel>('cases');
+    final allCases = box.values.toList();
+
+    allCases.sort((a, b) {
+      final aData = a.createdAt;
+      final bData = b.createdAt;
+      return b.createdAt.compareTo(a.createdAt);
+    });
+
+    if (_searchQuery.isEmpty) {
+      _filteredCases = allCases;
+    }
+    _filteredCases = allCases.where((clientCase) {
+      return clientCase.title
+              .toLowerCase()
+              .contains(_searchQuery.toLowerCase()) ||
+          clientCase.clientId.contains(_searchQuery) == true;
+    }).toList();
+  }
+
+  void updateSearchQuery(String query) {
+    _searchQuery = query;
     notifyListeners();
   }
 }
