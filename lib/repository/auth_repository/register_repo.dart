e7860e0f69de69a406505e980e8ad7e4f496c25b
mzee-flutter <mudair.khan37@gmail.com>
2025-09-15 11:19:46 +0400

    repository files are normally responsible for the api reqeust and get the response in term of UI-code flutter

---
+++ lib/repository/auth_repository/register_repo.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/foundation.dart';
+import 'package:right_case/data/base_api_service.dart';
+import 'package:right_case/data/network_api_service.dart';
+import 'package:right_case/models/auth_models/register_request_model.dart';
+import 'package:right_case/models/auth_models/user_model.dart';
+import 'package:right_case/resources/URLs/auth_urls.dart';
+
+class RegisterRepository {
+  final BaseApiServices _services = NetworkApiServices();
+
+  Future<UserModel> registerUser(RegisterRequestModel user) async {
+    final header = {
+      'accept': "application/json",
+      'Content-Type': 'application/json',
+    };
+
+    final requestBody = user.toJson();
+
+    try {
+      final response = await _services.getPostApiRequest(
+        "${AuthURL.baseURl}auth/register",
+        header,
+        requestBody,
+      );
+
+      final user = UserModel.fromJson(response);
+
+      return user;
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
+  }
+}
