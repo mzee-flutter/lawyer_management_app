63e16827042ca22d8e81041d504b7a8a0a3a1bf3
mzee-flutter <mudair.khan37@gmail.com>
2025-09-15 11:09:49 +0400

    converting the register reqeust data to json

---
+++ lib/models/auth_models/register_request_model.dart
@@ -0,0 +1,15 @@
+class RegisterRequestModel {
+  String name;
+  String email;
+  String password;
+
+  RegisterRequestModel({
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+
+  Map<String, dynamic> toJson() {
+    return {'name': name, 'email': email, 'password': password};
+  }
+}
