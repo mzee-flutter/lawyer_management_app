45c837cbfd1c419598faab611561a5b041edbc41
mzee-flutter <mudair.khan37@gmail.com>
2025-09-15 11:17:47 +0400

    this is the login repo where we used to request to the backend

---
+++ lib/repository/auth_repository/login_repo.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:right_case/data/base_api_service.dart';
+import 'package:right_case/data/network_api_service.dart';
+import 'package:right_case/view_model/services/token_storage_service.dart';
+import 'package:right_case/models/auth_models/login_request_model.dart';
+import 'package:right_case/resources/URLs/auth_urls.dart';
+
+class LoginRepository {
+  final BaseApiServices _services = NetworkApiServices();
+  final TokenStorageService _tokenStorage = TokenStorageService();
+
+  Future<bool> loginUser(LoginRequestModel user) async {
+    final header = {'Content-Type': 'application/json'};
+    final requestBody = user.toJson();
+    try {
+      final response = await _services.getPostApiRequest(
+        "${AuthURL.baseURl}auth/login",
+        header,
+        requestBody,
+      );
+
+      final accessToken = response["access_token"];
+      final refreshToken = response["refresh_token"];
+      final accessTokenExpiry = response["expire_at"];
+
+      await _tokenStorage.saveToken(
+        accessToken,
+        refreshToken,
+        accessTokenExpiry,
+      );
+
+      // final user = TokenModel.fromJson(response);
+
+      return true;
+    } catch (e) {
+      debugPrint(e.toString());
+      rethrow;
+    }
+  }
+}
