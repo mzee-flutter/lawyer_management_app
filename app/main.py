e963d9c9e6f73cbe0e4ccb60da46ecc75acee7bd
mzee-flutter <mudair.khan37@gmail.com>
2025-09-16 11:29:46 +0400

    added the me route

--- app/main.py
+++ app/main.py
@@ -1,7 +1,25 @@
+from fastapi import FastAPI
+from app.database.base import Base
+from app.database.init_db import engine
+from contextlib import asynccontextmanager
+from app.routers import (auth_routers)
+from app.models import auth_model
+
+
+
+#this is the startup function that ensure that the table are created which we have define it in the model
+async def lifespan(app=FastAPI):
+    Base.metadata.create_all(bind= engine)
+
+    yield
 
 
 
 
+
+app= FastAPI(title="Lawyer App Backend", lifespan=lifespan, docs_url="/docs", redoc_url="/redoc", openapi_url="/openai.json")
+
+app.include_router(auth_routers.router)
 
 
 
