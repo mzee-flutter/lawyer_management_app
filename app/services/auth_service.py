f42c75f595d0a957ded3fd72fc8cdfe4474a04a9
mzee-flutter <mudair.khan37@gmail.com>
2025-09-16 11:32:26 +0400

    this file contain the validation and exception all together and define the function for a specific functionality

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,12 +1,16 @@
 from sqlalchemy.orm import Session
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer
 from datetime import datetime, timezone, timedelta, UTC
 import os
 
+from app.database.session import get_db
 from app.repositories import auth_repository
-from app.schemas.auth_schema import UserCreate, UserLogin, TokenResponse
+from app.schemas.auth_schema import UserPublic, UserCreate, UserLogin, TokenResponse
 from app.utils.auth_utils import hash_password, verify_password, _sha256, create_access_token, create_refresh_token, decode_accesss_token
 
+
+auth2_scheme= OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 
 
@@ -27,6 +31,19 @@ def login_user(db:Session, user:UserLogin):
 
 
 
+def get_current_user( db:Session=Depends(get_db), token:str= Depends(auth2_scheme)):
+    payload= decode_accesss_token(token)
+
+    if not payload:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Invalid or expired token",
+                            headers={"WWW-Authenticate": "Bearer"})
+    user= auth_repository.get_user_by_id(db, int(payload["sub"]))
+    if not user:
+        raise HTTPException(detail="Current User Not Found")
+    return user
+
+
 def generate_tokens(db:Session, user):
     payload= {"sub":str(user.id)}
 
@@ -36,7 +53,8 @@ def generate_tokens(db:Session, user):
     refresh_expire_at= datetime.now(UTC) + timedelta(days=int(os.getenv("REFRESH_TOKEN_EXPIRY_DAY")))
 
     auth_repository.save_refresh_token(db, user.id, hash_refresh, refresh_expire_at)
-    tokens= TokenResponse(access_token=access_token, refresh_token=raw_refresh, expire_at=expire_in)
+    expire_at_unix= int(expire_in.timestamp())
+    tokens= TokenResponse(access_token=access_token, refresh_token=raw_refresh, expire_at= expire_at_unix)
     return tokens
 
     
@@ -69,8 +87,8 @@ def revoke_refresh(db:Session, raw_refresh:str):
     if not record:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid refresh token")
 
-    if record.revoker:
+    if record.revoked:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Token already expired")
     
     auth_repository.revoke_refresh_token(db, record)
-    return {"message": "The user is deleted Successfully"}
+    return {"message": "The user is logout Successfully"}
