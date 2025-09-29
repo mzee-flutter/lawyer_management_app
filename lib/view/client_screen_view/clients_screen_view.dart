aebbd4566dd2e12789feb9e8a6765d677f236120
mzee-flutter <mudair.khan37@gmail.com>
2025-09-29 20:24:30 +0400

    now we are going to use the endpoint for the searching clients

--- lib/view/client_screen_view/clients_screen_view.dart
+++ lib/view/client_screen_view/clients_screen_view.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
 import 'package:provider/provider.dart';
-
 import 'package:right_case/resources/client_info_card.dart';
 import 'package:right_case/utils/routes/routes_names.dart';
-
-import 'package:right_case/view_model/client_view_model.dart';
+import 'package:right_case/view_model/client_view_model/client_list_view_model.dart';
 
 class ClientsScreen extends StatefulWidget {
   const ClientsScreen({super.key});
@@ -17,9 +14,17 @@ class ClientsScreen extends StatefulWidget {
 
 class _ClientsScreenState extends State<ClientsScreen> {
   @override
-  Widget build(BuildContext context) {
-    final clientVM = Provider.of<ClientViewModel>(context);
+  void initState() {
+    final clientListVM =
+        Provider.of<ClientListViewModel>(context, listen: false);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      clientListVM.fetchClientList();
+    });
+    super.initState();
+  }
 
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         elevation: 3,
@@ -31,60 +36,62 @@ class _ClientsScreenState extends State<ClientsScreen> {
         child: Column(
           children: [
             TextField(
-                decoration: InputDecoration(
-                  hintText: 'Search Clients...',
-                  prefixIcon: const Icon(Icons.search),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.r),
-                  ),
+              decoration: InputDecoration(
+                hintText: 'Search Clients...',
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.r),
                 ),
-                onChanged: (query) {
-                  clientVM.updateSearchQuery(query);
-                }),
-            SizedBox(height: 12.h),
-            Consumer<ClientViewModel>(builder: (context, clientVM, child) {
-              final filteredClients = clientVM.filteredClients;
-              if (filteredClients.isEmpty) {
-                return Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 100.h,
-                        width: 100.w,
-                        decoration: BoxDecoration(
-                            color: Colors.grey.shade300,
-                            shape: BoxShape.circle),
-                        child: Icon(
-                          Icons.group_off_outlined,
-                          size: 40,
-                          color: Colors.grey.shade500,
-                        ),
-                      ),
-                      Center(
-                        child: Text(
-                          'Client Not Found',
-                          style: TextStyle(
+              ),
+            ),
+            Consumer<ClientListViewModel>(
+              builder: (BuildContext context, clientListVM, Widget? child) {
+                if (clientListVM.isLoading) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                if (clientListVM.clientList.isEmpty) {
+                  return Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 100.h,
+                          width: 100.w,
+                          decoration: BoxDecoration(
+                              color: Colors.grey.shade300,
+                              shape: BoxShape.circle),
+                          child: Icon(
+                            Icons.group_off_outlined,
+                            size: 40,
                             color: Colors.grey.shade500,
                           ),
                         ),
-                      ),
-                    ],
+                        Center(
+                          child: Text(
+                            'Client Not Found',
+                            style: TextStyle(
+                              color: Colors.grey.shade500,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }
+
+                return Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 12.r),
+                    child: ListView.builder(
+                        itemCount: clientListVM.clientList.length,
+                        itemBuilder: (context, index) {
+                          final client = clientListVM.clientList[index];
+                          return ClientInfoCard(client: client);
+                        }),
                   ),
                 );
-              }
-              return Expanded(
-                child: ListView.builder(
-                  itemCount: filteredClients.length,
-                  itemBuilder: (context, index) {
-                    final client = filteredClients[index];
-                    return ClientInfoCard(
-                      client: client,
-                    );
-                  },
-                ),
-              );
-            }),
+              },
+            ),
           ],
         ),
       ),
