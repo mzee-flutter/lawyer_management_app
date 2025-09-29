73a53c171c3110bf9e7bd6194540f1fb64acf9c1
mzee-flutter <mudair.khan37@gmail.com>
2025-09-29 12:48:04 +0400

    formating the number the for phone sms and whatsapp

--- lib/view_model/services/contact_service.dart
+++ lib/view_model/services/contact_service.dart
@@ -2,13 +2,23 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class ContactService {
+  String formatNumber(String rawNumber) {
+    String digits = rawNumber.replaceAll(RegExp(r'\D'), '');
+    if (digits.startsWith('0')) {
+      digits = '92${digits.substring(1)}'; // Pakistan example
+    }
+    return digits;
+  }
+
   void makePhoneCall(BuildContext context, String? phoneNumber) async {
     if (phoneNumber == null || phoneNumber.trim().isEmpty) {
       _showSnackBar(context, 'Phone number not available');
       return;
     }
 
-    final Uri phoneUri = Uri(scheme: 'tel', path: phoneNumber);
+    final cleanNumber = formatNumber(phoneNumber);
+    final Uri phoneUri = Uri(scheme: 'tel', path: cleanNumber);
+
     if (await canLaunchUrl(phoneUri)) {
       await launchUrl(phoneUri, mode: LaunchMode.externalApplication);
     } else {
@@ -22,7 +32,9 @@ class ContactService {
       return;
     }
 
-    final Uri smsUri = Uri(scheme: 'sms', path: phoneNumber);
+    final cleanNumber = formatNumber(phoneNumber);
+    final Uri smsUri = Uri(scheme: 'sms', path: cleanNumber);
+
     if (await canLaunchUrl(smsUri)) {
       await launchUrl(smsUri, mode: LaunchMode.externalApplication);
     } else {
@@ -36,8 +48,9 @@ class ContactService {
       return;
     }
 
-    // Format: https://wa.me/<number> or https://api.whatsapp.com/send?phone=<number>
-    final Uri whatsappUri = Uri.parse("https://wa.me/$phoneNumber");
+    final cleanNumber = formatNumber(phoneNumber);
+    final Uri whatsappUri = Uri.parse("https://wa.me/$cleanNumber");
+
     if (await canLaunchUrl(whatsappUri)) {
       await launchUrl(whatsappUri, mode: LaunchMode.externalApplication);
     } else {
@@ -51,5 +64,3 @@ class ContactService {
     );
   }
 }
-
-/// checking that contact works or not just running the code...
