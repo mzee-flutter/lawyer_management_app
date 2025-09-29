6344b4b78370b183220c7522270215276f0cb44c
mzee-flutter <mudair.khan37@gmail.com>
2025-09-29 22:21:04 +0400

    this is all about the cases category buttons

--- lib/resources/custom_cases_category_view.dart
+++ lib/resources/custom_cases_category_view.dart
@@ -1,26 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:right_case/models/case_model.dart';
-import 'package:right_case/resources/case_info_card.dart';
 
 class CustomCasesCategoryView extends StatelessWidget {
-  final String title;
-  final List<CaseModel> cases;
-
-  const CustomCasesCategoryView(
-      {super.key, required this.title, required this.cases});
+  const CustomCasesCategoryView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: cases.isEmpty
-          ? const Center(child: Text("No cases found."))
-          : ListView.builder(
-              itemCount: cases.length,
-              itemBuilder: (context, index) {
-                return CaseInfoCard(clientCase: cases[index]);
-              },
-            ),
-    );
+        appBar: AppBar(title: Text("customCasesCategoryView")),
+        body: const Center(child: Text("No cases found.")));
   }
 }
