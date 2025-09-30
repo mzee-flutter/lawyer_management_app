9cf1c7a1600fbefabdfe3e36b9a395037c5ca9de
mzee-flutter <mudair.khan37@gmail.com>
2025-09-30 21:53:55 +0400

    just updating the dependencies

--- lib/repository/client_repository/client_list_repo.dart
+++ lib/repository/client_repository/client_list_repo.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:right_case/data/base_api_service.dart';
 import 'package:right_case/data/network_api_service.dart';
-import 'package:right_case/models/client_model.dart';
+
+import 'package:right_case/models/client_models/client_model.dart';
 import 'package:right_case/resources/URLs/client_urls.dart';
 
 class ClientListRepo {
