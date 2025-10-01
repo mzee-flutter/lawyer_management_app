fbabce3025d7bb815636958cd7edf822c2a9e05e
mzee-flutter <mudair.khan37@gmail.com>
2025-10-01 21:32:54 +0400

    this is the view_model class that notify the UI and get the updated client from the repo and update the client

--- lib/view_model/client_view_model/client_update_view_model.dart
+++ lib/view_model/client_view_model/client_update_view_model.dart
@@ -1,36 +1,49 @@
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+import 'package:right_case/models/client_models/client_create_model.dart';
 import 'package:right_case/models/client_models/client_model.dart';
+import 'package:right_case/repository/client_repository/client_update_repo.dart';
+import 'package:right_case/view_model/client_view_model/client_list_view_model.dart';
 
-class ClientEditViewModel with ChangeNotifier {
+class ClientUpdateViewModel with ChangeNotifier {
+  final ClientUpdateRepo _clientUpdateRepo = ClientUpdateRepo();
   late TextEditingController nameController;
-  late TextEditingController phoneController;
   late TextEditingController emailController;
+  late TextEditingController phoneController;
+  late TextEditingController cnicController;
   late TextEditingController addressController;
+  late TextEditingController notesController;
 
-  late ClientModel _originalClient;
+  // late ClientModel _originalClient;
 
   void initializeFields(ClientModel client) {
-    _originalClient = client;
+    // _originalClient = client;
     nameController = TextEditingController(text: client.name);
+    emailController = TextEditingController(text: client.email);
     phoneController = TextEditingController(text: client.phone);
-    emailController = TextEditingController(text: client.email ?? '');
-    addressController = TextEditingController(text: client.address ?? '');
+    cnicController = TextEditingController(text: client.cnic);
+    addressController = TextEditingController(text: client.address);
+    notesController = TextEditingController(text: client.notes);
   }
 
-  void saveChanges(context) {
-    // final updatedClient = ClientModel(
-    //   id: _originalClient.id,
-    //   name: nameController.text.trim(),
-    //   phone: phoneController.text.trim(),
-    //   email: emailController.text.trim(),
-    //   address: addressController.text.trim(),
-    //   createdAt: _originalClient.createdAt,
-    // );
+  Future<void> saveChanges(context, String id) async {
+    final updatedClient = ClientCreateModel(
+      name: nameController.text.trim(),
+      email: emailController.text.trim(),
+      phone: phoneController.text.trim(),
+      cnic: cnicController.text.trim(),
+      address: addressController.text.trim(),
+      notes: notesController.text.trim(),
+    );
 
-    // Provider.of<ClientViewModel>(context, listen: false)
-    //     .updateClient(updatedClient);
-
-    Navigator.pop(context);
+    try {
+      final dbClient = await _clientUpdateRepo.clientUpdate(updatedClient, id);
+      Provider.of<ClientListViewModel>(context, listen: false)
+          .updateClient(dbClient);
+      Navigator.pop(context);
+    } catch (e) {
+      debugPrint("Error in ClientUpdateViewModel: $e");
+    }
   }
 
   void disposeControllersOnly() {
