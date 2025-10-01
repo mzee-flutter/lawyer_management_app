ccbb8b9ff83007a9c36057b3e68ea1d5d93c31bc
mzee-flutter <mudair.khan37@gmail.com>
2025-10-01 23:22:57 +0400

    this is where we request for the soft delete not deleting the client permenantly

---
+++ lib/repository/client_repository/client_archive_repo.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:right_case/data/base_api_service.dart';
+import 'package:right_case/data/network_api_service.dart';
+
+import 'package:right_case/models/client_models/client_model.dart';
+import 'package:right_case/resources/URLs/client_urls.dart';
+
+class ClientArchiveRepo {
+  final BaseApiServices _services = NetworkApiServices();
+  Future<ClientModel> archiveClient(String id) async {
+    try {
+      final response = await _services.getDeleteApiRequest(
+          "${ClientURl.baseUrl}$id", ClientURl.headers);
+
+      final clients = ClientModel.fromJson(response);
+      return clients;
+    } catch (e) {
+      debugPrint("Error in ClientListRepo: $e");
+      rethrow;
+    }
+  }
+}
