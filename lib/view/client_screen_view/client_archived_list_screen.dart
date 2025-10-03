9f4f07cf77a9831354dacd88a73e45d7d97b5b14
mzee-flutter <mudair.khan37@gmail.com>
2025-10-03 22:17:46 +0400

    this is screen where we show the list of the archive client by scolling the list we get more clients as well

---
+++ lib/view/client_screen_view/client_archived_list_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
+import 'package:right_case/resources/client_info_card.dart';
+import 'package:right_case/view_model/client_view_model/client_archived_list_view_model.dart';
+
+class ClientArchivedListScreen extends StatefulWidget {
+  const ClientArchivedListScreen({super.key});
+
+  @override
+  State<ClientArchivedListScreen> createState() =>
+      _ClientArchivedListScreenState();
+}
+
+class _ClientArchivedListScreenState extends State<ClientArchivedListScreen> {
+  @override
+  void initState() {
+    super.initState();
+    final clientListVM =
+        Provider.of<ClientArchivedListViewModel>(context, listen: false);
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      clientListVM.fetchArchivedClients();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 3,
+        backgroundColor: Colors.grey.shade300,
+        title: const Text("Archive Clients"),
+      ),
+      body: Consumer<ClientArchivedListViewModel>(
+        builder: (BuildContext context, clientListVM, Widget? child) {
+          if (clientListVM.isFirstLoading) {
+            // Show full loader on first fetch
+            return Center(
+              child: CircularProgressIndicator(
+                color: Colors.grey.shade700,
+                strokeWidth: 2,
+              ),
+            );
+          }
+
+          if (clientListVM.archiveClientList.isEmpty) {
+            return Center(
+              child: Padding(
+                padding: EdgeInsets.all(16.r),
+                child: const Text(
+                  "No archived clients found.",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            );
+          }
+
+          return NotificationListener<ScrollNotification>(
+            onNotification: (scrollInfo) {
+              if (scrollInfo.metrics.pixels >=
+                  scrollInfo.metrics.maxScrollExtent * 0.85) {
+                if (!clientListVM.isMoreLoading && clientListVM.hasMore) {
+                  clientListVM.fetchArchivedClients(loadMore: true);
+                }
+              }
+              return false;
+            },
+            child: ListView.builder(
+              padding: EdgeInsets.all(12.r),
+              itemCount: clientListVM.archiveClientList.length +
+                  (clientListVM.isMoreLoading ? 1 : 0),
+              itemBuilder: (context, index) {
+                if (index < clientListVM.archiveClientList.length) {
+                  final client = clientListVM.archiveClientList[index];
+                  return ClientInfoCard(client: client);
+                } else {
+                  // Loader at bottom for loadMore
+                  return Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16.r),
+                    child: Center(
+                      child: CircularProgressIndicator(
+                        color: Colors.grey.shade700,
+                        strokeWidth: 2,
+                      ),
+                    ),
+                  );
+                }
+              },
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
