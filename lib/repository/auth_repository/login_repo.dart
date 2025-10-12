3fd2955b08cd1205cb8825cbae4301f6a16fa750
mzee-flutter <mudair.khan37@gmail.com>
2025-10-12 22:28:32 +0400

    updated according to the model

--- lib/repository/auth_repository/login_repo.dart
+++ lib/repository/auth_repository/login_repo.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:right_case/data/base_api_service.dart';
 import 'package:right_case/data/network_api_service.dart';
+import 'package:right_case/models/auth_models/auth_model.dart';
 import 'package:right_case/view_model/services/token_storage_service.dart';
 import 'package:right_case/models/auth_models/login_request_model.dart';
 import 'package:right_case/resources/URLs/auth_urls.dart';
@@ -9,7 +10,7 @@ class LoginRepository {
   final BaseApiServices _services = NetworkApiServices();
   final TokenStorageService _tokenStorage = TokenStorageService();
 
-  Future<bool> loginUser(LoginRequestModel user) async {
+  Future<AuthModel> loginUser(LoginRequestModel user) async {
     final header = {'Content-Type': 'application/json'};
     final requestBody = user.toJson();
     try {
@@ -18,10 +19,10 @@ class LoginRepository {
         header,
         requestBody,
       );
-
-      final accessToken = response["access_token"] as String;
-      final refreshToken = response["refresh_token"] as String;
-      final accessTokenExpiry = response["expire_at"] as int;
+      final authResponse = AuthModel.fromJson(response);
+      final accessToken = authResponse.tokens.accessToken;
+      final refreshToken = authResponse.tokens.refreshToken;
+      final accessTokenExpiry = authResponse.tokens.expireAt;
 
       await _tokenStorage.saveToken(
         accessToken,
@@ -29,9 +30,7 @@ class LoginRepository {
         accessTokenExpiry,
       );
 
-      // final user = TokenModel.fromJson(response);
-
-      return true;
+      return authResponse;
     } catch (e) {
       debugPrint(e.toString());
       rethrow;
--- lib/repository/auth_repository/register_repo.dart
+++ lib/repository/auth_repository/register_repo.dart
@@ -2,13 +2,15 @@ import 'package:flutter/foundation.dart';
 import 'package:right_case/data/base_api_service.dart';
 import 'package:right_case/data/network_api_service.dart';
 import 'package:right_case/models/auth_models/register_request_model.dart';
-import 'package:right_case/models/auth_models/user_model.dart';
+import 'package:right_case/models/auth_models/auth_model.dart';
 import 'package:right_case/resources/URLs/auth_urls.dart';
+import 'package:right_case/view_model/services/token_storage_service.dart';
 
 class RegisterRepository {
   final BaseApiServices _services = NetworkApiServices();
+  final TokenStorageService _tokenStorage = TokenStorageService();
 
-  Future<UserModel> registerUser(RegisterRequestModel user) async {
+  Future<AuthModel> registerUser(RegisterRequestModel user) async {
     final header = {
       'accept': "application/json",
       'Content-Type': 'application/json',
@@ -23,9 +25,17 @@ class RegisterRepository {
         requestBody,
       );
 
-      final user = UserModel.fromJson(response);
+      final authResponse = AuthModel.fromJson(response);
+      final accessToken = authResponse.tokens.accessToken;
+      final refreshToken = authResponse.tokens.refreshToken;
+      final accessTokenExpiry = authResponse.tokens.expireAt;
 
-      return user;
+      await _tokenStorage.saveToken(
+        accessToken,
+        refreshToken,
+        accessTokenExpiry,
+      );
+      return authResponse;
     } catch (e) {
       debugPrint(e.toString());
       rethrow;
