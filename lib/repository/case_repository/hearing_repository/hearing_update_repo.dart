efb371a32e79134c2fc390680722b401f3bb9149
mzee-flutter <mudair.khan37@gmail.com>
2026-07-03 04:48:30 -0700

    updated according to the model

--- lib/repository/case_repository/hearing_repository/hearing_update_repo.dart
+++ lib/repository/case_repository/hearing_repository/hearing_update_repo.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:right_case/data/base_api_service.dart';
 import 'package:right_case/data/network_api_service.dart';
-import 'package:right_case/models/case_models/case_create_model.dart';
-import 'package:right_case/models/case_models/case_model.dart';
 import 'package:right_case/models/case_models/hearing_models.dart';
 import 'package:right_case/resources/URLs/case_urls.dart';
 
 class HearingUpdateRepo {
   final BaseApiServices _services = NetworkApiServices();
 
+  // Now takes HearingUpdateModel instead of HearingCreateModel
   Future<HearingPublicModel> updateHearing(
-    HearingCreateModel hearing,
+    HearingUpdateModel hearing,
     String hearingId,
   ) async {
     final requestBody = hearing.toJson();
@@ -20,11 +19,9 @@ class HearingUpdateRepo {
         CaseUrls.headers,
         requestBody,
       );
-
-      final dbHearing = HearingPublicModel.fromJson(response);
-      return dbHearing;
+      return HearingPublicModel.fromJson(response);
     } catch (e) {
-      debugPrint("Error in HearingUpdateRepo: $e");
+      debugPrint('Error in HearingUpdateRepo: $e');
       rethrow;
     }
   }
