e54257bf9d97b84504edc04bb37b1050f8eb13a2
mzee-flutter <mudair.khan37@gmail.com>
2026-08-06 22:25:33 +0500

    updated with user id so that the resources become user centric

--- app/services/client_service.py
+++ app/services/client_service.py
@@ -2,18 +2,20 @@ from sqlalchemy.orm import Session
 from fastapi import HTTPException, status
 from app.repositories.client_repository import ClientRepository
 from app.schemas.client_schema import ClientCreate, ClientUpdate, ClientPublic
+from app.models.case_model import CaseRelatedClient
+from uuid import UUID
 
 
 class ClientService:
 
     @staticmethod
-    def create_client(db: Session, client_in: ClientCreate) -> ClientPublic:
-        client = ClientRepository.create(db, client_in)
+    def create_client(db: Session, client_in: ClientCreate, user_id: UUID) -> ClientPublic:
+        client = ClientRepository.create(db, client_in, user_id)
         return ClientPublic.model_validate(client)
 
     @staticmethod
-    def get_client(db: Session, client_id) -> ClientPublic:
-        client = ClientRepository.get_by_id(db, client_id)
+    def get_client(db: Session, client_id, user_id: UUID) -> ClientPublic:
+        client = ClientRepository.get_by_id(db, client_id, user_id)
         if not client:
             raise HTTPException(status_code=404, detail="Client not found")
         return ClientPublic.model_validate(client)
@@ -21,31 +23,33 @@ class ClientService:
     @staticmethod
     def search_clients(
     db: Session,
+    user_id:UUID,
     q: str | None = None,
     page: int = 1,
     size: int = 10,
     sort: str | None = None
 ) -> list[ClientPublic]:
         skip = (page - 1) * size
-        clients = ClientRepository.search(db, query=q, skip=skip, limit=size, sort=sort)
+        clients = ClientRepository.search(db, user_id= user_id, query=q, skip=skip, limit=size, sort=sort)
         return [ClientPublic.model_validate(c) for c in clients]
     
     @staticmethod
     def search_archived_clients(
         db:Session,
+        user_id: UUID,
         q: str | None= None,
         page: int= 1,
         size: int = 10,
         sort: str |None= None,
         )->list[ClientPublic]:
         skip= (page - 1) * size
-        clients= ClientRepository.search_archived(db, query=q, skip=skip, limit=size, sort=sort)
+        clients= ClientRepository.search_archived(db, user_id=user_id, query=q, skip=skip, limit=size, sort=sort)
         return [ClientPublic.model_validate(c) for c in clients]
 
 
     @staticmethod
-    def restore_client(db, client_id: str) -> ClientPublic:
-        client = ClientRepository.get_by_id(db, client_id)
+    def restore_client(db, client_id: str, user_id: UUID ) -> ClientPublic:
+        client = ClientRepository.get_by_id(db, client_id, user_id)
         if not client:
             raise HTTPException(status_code=404, detail="Client not found")
 
@@ -58,8 +62,21 @@ class ClientService:
 
 
     @staticmethod
-    def delete_client_permanently(db:Session, client_id:str)-> ClientPublic:
-        client= ClientRepository.get_by_id(db, client_id)
+    def delete_client_permanently(
+        db:Session,
+        client_id:str,
+        user_id: UUID
+    )-> ClientPublic:
+        attached_client= db.query(CaseRelatedClient).filter(
+            CaseRelatedClient.client_id==client_id,
+            CaseRelatedClient.user_id==user_id
+            ).count()
+        if attached_client > 0:
+            raise HTTPException(
+                status_code= status.HTTP_400_BAD_REQUEST,
+                detail="Client is attached to cases and cannot be deleted."
+            )
+        client= ClientRepository.get_by_id(db, client_id, user_id)
 
         if not client:
             raise HTTPException(status_code=404, detail="Client not Found")
@@ -71,16 +88,16 @@ class ClientService:
 
 
     @staticmethod
-    def update_client(db: Session, client_id, client_in: ClientUpdate) -> ClientPublic:
-        client = ClientRepository.get_by_id(db, client_id)
+    def update_client(db: Session, client_id, client_in: ClientUpdate, user_id: UUID) -> ClientPublic:
+        client = ClientRepository.get_by_id(db, client_id, user_id)
         if not client:
             raise HTTPException(status_code=404, detail="Client not found")
         updated = ClientRepository.update(db, client, client_in)
         return ClientPublic.model_validate(updated)
 
     @staticmethod
-    def archive_client(db: Session, client_id) -> ClientPublic:
-        client = ClientRepository.get_by_id(db, client_id)
+    def archive_client(db: Session, client_id, user_id: UUID) -> ClientPublic:
+        client = ClientRepository.get_by_id(db, client_id, user_id)
         if not client:
             raise HTTPException(status_code=404, detail="Client not found")
         archived = ClientRepository.archive(db, client)
